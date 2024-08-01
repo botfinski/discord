8087cb951adc86d841fc68d34c698415825970a5
unknown <botfinski@gmail.com>
2024-08-02 01:34:35 +0200

    post message functionality added

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -3,6 +3,7 @@ import { redirect } from "next/navigation";
 import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
 import { ChatHeader } from "@/components/chat/chat-header";
+import { ChatInput } from "@/components/chat/chat-input";
 
 type ChannelIdPageProps = {
 	params: {
@@ -31,6 +32,16 @@ export const ChannelIdPage = async ({
 	return (
 		<div className="bg-white dark:bg-[#313338] flex flex-col h-full">
 			<ChatHeader name={channel.name} serverId={channel.serverId} type="channel" />
+			<div className="flex-1">Future Messages</div>
+			<ChatInput
+				name={channel.name}
+				type="channel"
+				apiUrl="/api/socket/messages"
+				query={{
+					channelId: channel.id,
+					serverId: channel.serverId,
+				}}
+			/>
 		</div>
 	);
 };
